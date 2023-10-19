a68d910607193f050829490eb3934d6520fe43e9
-e <raman.makarewicz@gmail.com>
2023-10-19 14:22:59 +0200

    Create SelectedVehicleUiModel.kt

---
+++ feature/home/src/main/java/com/apsl/glideapp/feature/home/screens/SelectedVehicleUiModel.kt
@@ -0,0 +1,39 @@
+package com.apsl.glideapp.feature.home.screens
+
+import androidx.compose.runtime.Immutable
+import com.apsl.glideapp.core.model.Vehicle
+import com.apsl.glideapp.core.util.maps.toLatLng
+import com.google.android.gms.maps.model.LatLng
+import kotlin.math.roundToInt
+
+@Immutable
+data class SelectedVehicleUiModel(
+    val id: String,
+    val code: String,
+    val range: Int,
+    val unlockingFee: Double,
+    val farePerMinute: Double,
+    val coordinates: LatLng,
+    val batteryState: BatteryState = BatteryState.Undefined
+)
+
+enum class BatteryState {
+    Undefined, Low, Half, Max
+}
+
+fun Vehicle.toSelectedVehicleUiModel(): SelectedVehicleUiModel {
+    return SelectedVehicleUiModel(
+        id = this.id,
+        code = this.code.toString().padStart(4, '0'),
+        range = (this.batteryCharge * 0.4).roundToInt(),
+        unlockingFee = this.unlockingFee,
+        farePerMinute = this.farePerMinute,
+        coordinates = this.coordinates.toLatLng(),
+        batteryState = when (this.batteryCharge) {
+            in 0..34 -> BatteryState.Low
+            in 35..84 -> BatteryState.Half
+            in 85..100 -> BatteryState.Max
+            else -> BatteryState.Undefined
+        }
+    )
+}
