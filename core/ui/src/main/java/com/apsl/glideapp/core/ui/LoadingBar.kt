f2a7f78c84f1f15577e051654c801cef27397303
-e <raman.makarewicz@gmail.com>
2023-10-19 14:18:13 +0200

    Improve LoadingBar

--- core/ui/src/main/java/com/apsl/glideapp/core/ui/LoadingBar.kt
+++ core/ui/src/main/java/com/apsl/glideapp/core/ui/LoadingBar.kt
@@ -5,53 +5,59 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.apsl.glideapp.core.ui.theme.GlideAppTheme
+import kotlin.math.roundToInt
 
 @Composable
-fun LoadingBar(modifier: Modifier = Modifier) {
-    val animated = rememberInfiniteTransition(label = "")
-    val circleRadius = remember { mutableStateOf(6.dp) }
+fun LoadingBar(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.surfaceVariant
+) {
+    val circleRadius = 8.dp
+    val circleRadiusPx = circleRadius.toPx()
     val size = remember { mutableStateOf(IntSize.Zero) }
-    val endPadding = LocalDensity.current.run { circleRadius.value.toPx() * 2 }
+
+    val animated = rememberInfiniteTransition(label = "")
     val offsetY = animated.animateFloat(
         initialValue = 0f,
-        targetValue = size.value.width.toFloat() - endPadding,
+        targetValue = size.value.width.toFloat() - circleRadiusPx * 2,
         animationSpec = infiniteRepeatable(
             animation = tween(durationMillis = 800),
             repeatMode = RepeatMode.Reverse
         ),
         label = ""
     )
-    Box(
-        modifier
-            .height(circleRadius.value * 2)
+
+    Canvas(
+        modifier = modifier
+            .height(circleRadius * 2)
             .onGloballyPositioned { size.value = it.size }
-            .drawBehind {
-                drawCircle(
-                    radius = circleRadius.value.toPx(),
-                    color = Color.White,
-                    center = Offset(
-                        x = circleRadius.value.toPx() + offsetY.value,
-                        y = this.size.height / 2
-                    )
-                )
+            .offset {
+                IntOffset(x = offsetY.value.roundToInt(), y = 0)
             }
-    )
+    ) {
+        drawCircle(
+            radius = circleRadiusPx,
+            color = color,
+            center = Offset(x = circleRadiusPx, y = this.size.height / 2)
+        )
+    }
 }
 
 @Preview
