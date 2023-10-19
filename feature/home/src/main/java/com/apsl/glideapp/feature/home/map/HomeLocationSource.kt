b0d299890de883d32fa87f6320637d9b62bfde47
-e <raman.makarewicz@gmail.com>
2023-10-19 14:19:55 +0200

    Update HomeLocationSource.kt

--- feature/home/src/main/java/com/apsl/glideapp/feature/home/map/HomeLocationSource.kt
+++ feature/home/src/main/java/com/apsl/glideapp/feature/home/map/HomeLocationSource.kt
@@ -1,8 +1,14 @@
+@file:Suppress("Unused")
+
 package com.apsl.glideapp.feature.home.map
 
 import android.location.Location
+import androidx.compose.runtime.Immutable
+import com.apsl.glideapp.core.model.UserLocation
+import com.apsl.glideapp.core.util.maps.toLocation
 import com.google.android.gms.maps.LocationSource
 
+@Immutable
 object HomeLocationSource : LocationSource {
 
     private var listener: LocationSource.OnLocationChangedListener? = null
@@ -17,5 +23,9 @@ object HomeLocationSource : LocationSource {
 
     fun onLocationChanged(location: Location) {
         listener?.onLocationChanged(location)
+    }
+
+    fun onLocationChanged(userLocation: UserLocation) {
+        listener?.onLocationChanged(userLocation.toLocation())
     }
 }
