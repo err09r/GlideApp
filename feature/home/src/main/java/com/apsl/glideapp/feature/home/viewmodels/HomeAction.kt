af9525d5e539cc4f4668cf7d7dec231bc43387f4
-e <raman.makarewicz@gmail.com>
2023-10-20 16:32:30 +0200

    Update Ride-related classes according to 'glideapp-common' artifact changes

--- feature/home/src/main/java/com/apsl/glideapp/feature/home/viewmodels/HomeAction.kt
+++ feature/home/src/main/java/com/apsl/glideapp/feature/home/viewmodels/HomeAction.kt
@@ -1,3 +1,5 @@
+@file:Suppress("Unused")
+
 package com.apsl.glideapp.feature.home.viewmodels
 
 import androidx.compose.runtime.Immutable
@@ -5,8 +7,8 @@ import androidx.compose.runtime.Immutable
 @Immutable
 sealed interface HomeAction {
     data object LogOut : HomeAction
-    data class StartRide(val rideId: String) : HomeAction
-    data class RestartUserLocation(val rideId: String) : HomeAction
+    data class StartRide(val rideId: String, val startDateTime: String) : HomeAction
+    data class RestartUserLocation(val rideId: String, val startDateTime: String) : HomeAction
     data object FinishRide : HomeAction
     data class Toast(val message: String) : HomeAction
     data object OpenLocationSettingsDialog : HomeAction
