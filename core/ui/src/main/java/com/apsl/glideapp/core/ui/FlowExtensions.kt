c58266fbd9927ca89f6715396850561bbbe80b41
Roman Makarewicz <raman.makarewicz@gmail.com>
2023-10-31 10:13:47 +0100

    Create strings.xml and refactor

--- core/ui/src/main/java/com/apsl/glideapp/core/ui/FlowExtensions.kt
+++ core/ui/src/main/java/com/apsl/glideapp/core/ui/FlowExtensions.kt
@@ -1,8 +1,7 @@
-@file:SuppressLint("Unused", "ComposableNaming", "StateFlowValueCalledInComposition")
+@file:Suppress("Unused", "ComposableNaming", "StateFlowValueCalledInComposition")
 
 package com.apsl.glideapp.core.ui
 
-import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
@@ -10,11 +9,11 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 @Composable
 fun <T> StateFlow<T>.collectWithLifecycle(
--- core/ui/src/main/java/com/apsl/glideapp/core/ui/PagingExtensions.kt
+++ core/ui/src/main/java/com/apsl/glideapp/core/ui/PagingExtensions.kt
@@ -1,6 +1,5 @@
 package com.apsl.glideapp.core.ui
 
-import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.paging.PagingData
@@ -8,7 +7,7 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import kotlinx.coroutines.flow.Flow
 
-@SuppressLint("ComposableNaming")
+@Suppress("ComposableNaming")
 @Composable
 fun <T : Any> Flow<PagingData<T>>.receiveAsLazyPagingItems(action: (LazyPagingItems<T>) -> Unit) {
     val lazyPagingItems = this.collectAsLazyPagingItems()
