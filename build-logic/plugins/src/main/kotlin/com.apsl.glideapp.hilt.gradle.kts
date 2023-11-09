baa33ebba9d9e34b8b548a1baa2d08dcf12d1e29
-e <raman.makarewicz@gmail.com>
2023-11-09 12:16:51 +0100

    Workaround to remove explicit 'implementation' import in com.apsl.glideapp.hilt.gradle.kts

--- build-logic/plugins/src/main/kotlin/com.apsl.glideapp.hilt.gradle.kts
+++ build-logic/plugins/src/main/kotlin/com.apsl.glideapp.hilt.gradle.kts
@@ -1,11 +1,9 @@
-import gradle.kotlin.dsl.accessors._7358b228c777673f552d03f9fcb098db.implementation
-
 plugins {
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
 }
 
 dependencies {
-    implementation(libs.dagger.hilt.android)
+    "implementation"(libs.dagger.hilt.android) // Workaround to remove explicit 'implementation' import
     ksp(libs.dagger.hilt.compiler)
 }
