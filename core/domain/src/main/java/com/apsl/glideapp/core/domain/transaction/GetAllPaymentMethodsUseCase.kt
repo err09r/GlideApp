1605783c82f5511ce2d39301817769f2fa89fe4a
-e <raman.makarewicz@gmail.com>
2023-11-11 22:44:10 +0100

    Refactor GetAllPaymentMethodsUseCase.kt

--- core/domain/src/main/java/com/apsl/glideapp/core/domain/transaction/GetAllPaymentMethodsUseCase.kt
+++ core/domain/src/main/java/com/apsl/glideapp/core/domain/transaction/GetAllPaymentMethodsUseCase.kt
@@ -4,6 +4,5 @@ import com.apsl.glideapp.core.model.PaymentMethod
 import javax.inject.Inject
 
 class GetAllPaymentMethodsUseCase @Inject constructor() {
-
     operator fun invoke() = runCatching { PaymentMethod.entries }
 }
