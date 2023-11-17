b76ea8b5c36f20cc0938d9d8521474f6da529cdb
Roman Makarewicz <raman.makarewicz@gmail.com>
2023-11-17 16:06:36 +0100

    Add 'contentPadding' parameters to GlideImage

--- core/ui/src/main/java/com/apsl/glideapp/core/ui/components/GlideImage.kt
+++ core/ui/src/main/java/com/apsl/glideapp/core/ui/components/GlideImage.kt
@@ -2,6 +2,9 @@ package com.apsl.glideapp.core.ui.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,13 +19,16 @@ import com.apsl.glideapp.core.ui.theme.GlideAppTheme
 fun GlideImage(
     @DrawableRes imageResId: Int,
     modifier: Modifier = Modifier,
-    size: DpSize = DpSize(120.dp, 120.dp)
+    size: DpSize = DpSize(120.dp, 120.dp),
+    contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
-    Image(
-        painter = painterResource(imageResId),
-        contentDescription = null,
-        modifier = modifier.size(size)
-    )
+    Box(modifier = modifier.padding(contentPadding)) {
+        Image(
+            painter = painterResource(imageResId),
+            contentDescription = null,
+            modifier = Modifier.size(size)
+        )
+    }
 }
 
 @Preview(showBackground = true)
