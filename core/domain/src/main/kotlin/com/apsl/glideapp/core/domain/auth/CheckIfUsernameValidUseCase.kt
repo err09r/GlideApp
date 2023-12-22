d981037f5d1ed27a69f5eabeb570d34f30e3f317
Roman Makarewicz <raman.makarewicz@gmail.com>
2023-12-22 12:16:57 +0100

    Update username validation pattern

--- core/domain/src/main/kotlin/com/apsl/glideapp/core/domain/auth/CheckIfUsernameValidUseCase.kt
+++ core/domain/src/main/kotlin/com/apsl/glideapp/core/domain/auth/CheckIfUsernameValidUseCase.kt
@@ -5,6 +5,6 @@ import javax.inject.Inject
 class CheckIfUsernameValidUseCase @Inject constructor() {
 
     operator fun invoke(username: String): Boolean {
-        return username.matches(Regex("^[a-z0-9_-]{4,20}\$"))
+        return username.matches(Regex("^[a-z0-9_.-]{4,20}\$"))
     }
 }
