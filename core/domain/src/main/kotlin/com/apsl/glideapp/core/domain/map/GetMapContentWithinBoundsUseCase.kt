558d5d078c66f4451836e22296042c857261c76d
Roman Makarewicz <raman.makarewicz@gmail.com>
2024-02-24 14:41:27 +0100

    Update GetMapContentWithinBoundsUseCase

--- core/domain/src/main/kotlin/com/apsl/glideapp/core/domain/map/GetMapContentWithinBoundsUseCase.kt
+++ core/domain/src/main/kotlin/com/apsl/glideapp/core/domain/map/GetMapContentWithinBoundsUseCase.kt
@@ -1,13 +1,23 @@
 package com.apsl.glideapp.core.domain.map
 
 import com.apsl.glideapp.common.models.CoordinatesBounds
+import com.apsl.glideapp.core.domain.zone.ZoneRepository
 import javax.inject.Inject
+import kotlinx.coroutines.flow.firstOrNull
 
 class GetMapContentWithinBoundsUseCase @Inject constructor(
-    private val mapRepository: MapRepository
+    private val mapRepository: MapRepository,
+    private val zoneRepository: ZoneRepository
 ) {
 
     suspend operator fun invoke(bounds: CoordinatesBounds) = runCatching {
-        mapRepository.getMapContentWithinBounds(bounds)
+        // Determine whether send a request or skip
+        val shouldLoadContent = zoneRepository.getAllZones().firstOrNull()?.any { zone ->
+            zone.coordinates.any { bounds.contains(it) }
+        } ?: true
+
+        if (shouldLoadContent) {
+            mapRepository.getMapContentWithinBounds(bounds)
+        }
     }
 }
